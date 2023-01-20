e5b9a5c5dccda97b7840694905be04f730f93fd8
nlambert18 <91149071+nlambert18@users.noreply.github.com>
2023-01-20 13:54:31 -0700

    1/20

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 const bodyParser = require ('body-parser');
 
+const {v4: uuidv4} =require('uuid');
+
 app.use(bodyParser.json()); //application middleware, looks for incoming data
 
 app.get("/", (req, res) => {
@@ -14,9 +16,16 @@ app.get("/", (req, res) => {
 
 app.post('/login', (req, res) => {
     const loginUser = req.body.userName;
+    const loginPassword = req.body.password; //access password
     console.log(req.body);
     console.log('Login username:'+loginUser);
-    res.send('Hello '+loginUser);
+    if (loginUser=="Amireal18@gmail.com" && loginPassword=="Iamreal!8"){
+        const loginToken = uuidv4();
+        res.send(loginToken);
+    } else {
+        res.status(401);
+        res.send('Incorrect password for '+loginUser);
+    }
 });
 
 app.listen(port, () => {
