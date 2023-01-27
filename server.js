cbe4a1c27845dac0f30a5733a259976d425b0888
nlambert18 <91149071+nlambert18@users.noreply.github.com>
2023-01-27 13:46:39 -0700

    a lot

--- server.js
+++ server.js
@@ -8,18 +8,25 @@ const bodyParser = require ('body-parser');
 
 const {v4: uuidv4} =require('uuid');
 
+const Redis = require('redis');
+
+const redisClient = Redis.createClient({url:"redis://127.0.0.1:6379"});
+
 app.use(bodyParser.json()); //application middleware, looks for incoming data
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.send("Hello Nathan!");
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async(req, res) => {
     const loginUser = req.body.userName;
     const loginPassword = req.body.password; //access password
     console.log(req.body);
     console.log('Login username:'+loginUser);
-    if (loginUser=="Amireal18@gmail.com" && loginPassword=="Iamreal!8"){
+    const correctPassword = await redisClient.hGet('UserMap', loginUser);
+    if (loginPassword==correctPassword){
         const loginToken = uuidv4();
         res.send(loginToken);
     } else {
@@ -29,5 +36,7 @@ app.post('/login', (req, res) => {
 });
 
 app.listen(port, () => {
+    redisClient.connect();
     console.log("listening");
 });
+
